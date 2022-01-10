a66807fd817d9be02233ea680422f2c54800406c
Jenushanth <sivakumarjenushanth@gmail.com>
2022-01-10 20:26:49 +0530

    Initial Commit

---
+++ admin_panel/src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./topbar.css"
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarwrapper">
+                <div className="topLeft">
+                    <span className="logo">Admin Panel</span>
+                </div>
+                <div className="topRight">right</div>
+            </div>
+        </div>
+    )
+}
