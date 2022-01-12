9873b69c903a3e9c7decef2ee10b85a559adc737
Jenushanth <sivakumarjenushanth@gmail.com>
2022-01-12 20:05:28 +0530

    Top bar is completed and Works for Sidbar is started

--- admin_panel/src/App.js
+++ admin_panel/src/App.js
@@ -1,10 +1,16 @@
 import Topbar from "./components/topbar/Topbar";
-
+import Sidebar from "./components/sidebar/sidebar";
 
 function App() {
   return (
     <div >
       <Topbar/>
+      <div className="container">
+      <Sidebar/>
+      otherpages
+      </div>
+     
+
     </div>
   );
 }
--- admin_panel/src/components/topbar/Topbar.jsx
+++ admin_panel/src/components/topbar/Topbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./topbar.css"
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LanguageIcon from '@mui/icons-material/Language';
 export default function Topbar() {
     return (
         <div className="topbar">
@@ -7,7 +10,20 @@ export default function Topbar() {
                 <div className="topLeft">
                     <span className="logo">Admin Panel</span>
                 </div>
-                <div className="topRight">right</div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNoneIcon />
+                        <span className="topiconbadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <LanguageIcon />
+                        <span className="topiconbadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <SettingsIcon />   
+                    </div>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLQUXqMrzrmkxd3QpxGL5bzgxELsztrL1AgQ&usqp=CAU" alt="ProfilePicture" className="topAvatar" />
+                </div>
             </div>
         </div>
     )
