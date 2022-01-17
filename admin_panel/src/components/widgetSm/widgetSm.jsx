f362f9619f0042cbdc68007a524ed2cbb974196b
Jenushanth <sivakumarjenushanth@gmail.com>
2022-01-17 10:25:17 +0530

    Chart works done and Newly joined widget is created

---
+++ admin_panel/src/components/widgetSm/widgetSm.jsx
@@ -0,0 +1,77 @@
+import "./widgetSm.css"
+import {Visibility} from '@mui/icons-material';
+export default function widgetSm() {
+    return (
+        <div className="widgetSm">
+            <span className="widgetSmTitle">New Joined Members</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://1.bp.blogspot.com/-tMP_Ft8dupU/WijWV1WmFPI/AAAAAAAAB88/PjXIr0z4JwYj_Bw4K-eEsA33Z_QQoIh2wCLcBGAs/s400/stylish%2Bgirl%2Bpic%2Bfor%2Bfacebook%2Bprofile.jpg" alt="profile for small widgets" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">K.Anjana </span>
+                        <span className="widgetSmUserTitle">software engineer </span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://1.bp.blogspot.com/-tMP_Ft8dupU/WijWV1WmFPI/AAAAAAAAB88/PjXIr0z4JwYj_Bw4K-eEsA33Z_QQoIh2wCLcBGAs/s400/stylish%2Bgirl%2Bpic%2Bfor%2Bfacebook%2Bprofile.jpg" alt="profile for small widgets" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">K.Anjana </span>
+                        <span className="widgetSmUserTitle">software engineer </span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://1.bp.blogspot.com/-tMP_Ft8dupU/WijWV1WmFPI/AAAAAAAAB88/PjXIr0z4JwYj_Bw4K-eEsA33Z_QQoIh2wCLcBGAs/s400/stylish%2Bgirl%2Bpic%2Bfor%2Bfacebook%2Bprofile.jpg" alt="profile for small widgets" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">K.Anjana </span>
+                        <span className="widgetSmUserTitle">software engineer </span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://1.bp.blogspot.com/-tMP_Ft8dupU/WijWV1WmFPI/AAAAAAAAB88/PjXIr0z4JwYj_Bw4K-eEsA33Z_QQoIh2wCLcBGAs/s400/stylish%2Bgirl%2Bpic%2Bfor%2Bfacebook%2Bprofile.jpg" alt="profile for small widgets" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">K.Anjana </span>
+                        <span className="widgetSmUserTitle">software engineer </span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://1.bp.blogspot.com/-tMP_Ft8dupU/WijWV1WmFPI/AAAAAAAAB88/PjXIr0z4JwYj_Bw4K-eEsA33Z_QQoIh2wCLcBGAs/s400/stylish%2Bgirl%2Bpic%2Bfor%2Bfacebook%2Bprofile.jpg" alt="profile for small widgets" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">K.Anjana </span>
+                        <span className="widgetSmUserTitle">software engineer </span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display 
+                    </button>
+                </li>
+                <li className="widgetSmListItem">
+                    <img src="https://1.bp.blogspot.com/-tMP_Ft8dupU/WijWV1WmFPI/AAAAAAAAB88/PjXIr0z4JwYj_Bw4K-eEsA33Z_QQoIh2wCLcBGAs/s400/stylish%2Bgirl%2Bpic%2Bfor%2Bfacebook%2Bprofile.jpg" alt="profile for small widgets" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUserName">K.Anjana </span>
+                        <span className="widgetSmUserTitle">software engineer </span>
+                    </div>
+                    <button className="widgetSmButton">
+                        <Visibility className="widgetSmIcon"/>
+                        Display 
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
--- admin_panel/src/pages/home/Home.jsx
+++ admin_panel/src/pages/home/Home.jsx
@@ -2,11 +2,19 @@
 import "./home.css";
 import Featuredinfo from "../../components/featuredInfo/featuredinfo";
 import Chart from "../../components/chart/Chart";
+import {userData} from "../../dummyData"
+import WidgetLg from "../../components/widgetLg/widgetLg";
+import WidgetSm from "../../components/widgetSm/widgetSm"
 export default function Home() {
+    // console.log(userData);
     return (
         <div className="home">
             <Featuredinfo/>
-            <Chart/>
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+            <div className="homeWidgets">
+                <WidgetSm/>
+                <WidgetLg/>
+            </div>
         </div>
     )
 }
