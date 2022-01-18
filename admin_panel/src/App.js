42df3b013e2e6dd4e3f8f5ed238c476cdf90fd41
Jenushanth <sivakumarjenushanth@gmail.com>
2022-01-18 19:52:45 +0530

    Large widget works done and user part works started

--- admin_panel/src/App.js
+++ admin_panel/src/App.js
@@ -1,20 +1,23 @@
+import React from "react";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/userList";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+// import { Switch } from "@mui/material";
 
 function App() {
   return (
-    <div >
+    <BrowserRouter>
       <Topbar />
       {/* container is created for displaying two widgets(sidebar,otherpages) in row manner */}
-      <div className="container">  
+      <div className="container">
         <Sidebar />
-        <Home/>
+            <Home />
+            <UserList/>
       </div>
-
-
-    </div>
+    </BrowserRouter>
   );
 }
 
--- admin_panel/src/components/widgetLg/widgetLg.jsx
+++ admin_panel/src/components/widgetLg/widgetLg.jsx
@@ -1,9 +1,65 @@
 import "./widgetLg.css"
 
 export default function widgetLg() {
+    const Button=({type})=>{
+        return <button className={"widgetLgBtn "+ type}>{type}</button>
+    }
     return (
         <div className="widgetLg">
-            Large
+            <h3 className="widgetLgTitle">Latest Transactions</h3>
+            <table className="widgetLgTable">
+                <tr className="widgetLgTr">
+                    <th className="widgetLgTh">Customer</th>
+                    <th className="widgetLgTh">Date</th>
+                    <th className="widgetLgTh">Amount</th>
+                    <th className="widgetLgTh">Status</th>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Jenushanth</span>
+                    </td>
+                    <td className="widgetLgDate">1 Feb 2022</td>
+                    <td className="widgetLgAmount">$100</td>
+                    <td className="widgetLgStatus"><Button type="Approved"/></td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="img1" className="widgetLgImg" />
+                        <span className="widgetLgName">Jenushanth</span>
+                    </td>
+                    <td className="widgetLgDate">1 Feb 2022</td>
+                    <td className="widgetLgAmount">$100</td>
+                    <td className="widgetLgStatus"><Button type="Declined"/></td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="img2" className="widgetLgImg" />
+                        <span className="widgetLgName">Jenushanth</span>
+                    </td>
+                    <td className="widgetLgDate">1 Feb 2022</td>
+                    <td className="widgetLgAmount">$100</td>
+                    <td className="widgetLgStatus"><Button type="Pending"/></td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="img3" className="widgetLgImg" />
+                        <span className="widgetLgName">Jenushanth</span>
+                    </td>
+                    <td className="widgetLgDate">1 Feb 2022</td>
+                    <td className="widgetLgAmount">$100</td>
+                    <td className="widgetLgStatus"><Button type="Approved"/></td>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="img4" className="widgetLgImg" />
+                        <span className="widgetLgName">Jenushanth</span>
+                    </td>
+                    <td className="widgetLgDate">1 Feb 2022</td>
+                    <td className="widgetLgAmount">$100</td>
+                    <td className="widgetLgStatus"><Button type="Approved"/></td>
+                </tr>
+            </table>
         </div>
     )
 }
